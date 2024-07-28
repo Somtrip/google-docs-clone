3258cfd11f426f32a1081a697d1022a629b81304
Som Tripathi <95291887+Somtrip@users.noreply.github.com>
2024-07-29 03:06:24 +0530

    mongodb uri update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const Document= require("./Document")
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, {  });
+mongoose.connect(`${MONGODB_URI}`)
+.then( () => console.log("mongodb connected"));
 
 const io = require('socket.io')(PORT, {
     cors: {
